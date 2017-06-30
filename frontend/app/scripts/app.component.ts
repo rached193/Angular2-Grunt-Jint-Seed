9045404cee5047ffbb6899eab0c5b4a1016a9bad
rached193 <goldensun193>
2017-06-30 16:54:32 +0200

    Añadir routas, y vistas parciales

--- frontend/app/scripts/app.component.ts
+++ frontend/app/scripts/app.component.ts
@@ -4,6 +4,7 @@ import {Observable} from 'rxjs/Rx';
 /* Dependecias de Angular */
 import {Component, NgModule} from '@angular/core'; // NgModule -> Modulo Raiz de la aplicacion
 import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule}   from '@angular/router';
 
 /* Depedencias de la Aplicacion */
 import {HeaderComponent} from './components/header.component'
@@ -11,17 +12,34 @@ import {FooterComponent} from './components/footer.component'
 import {AddDataComponent} from './section/addData/addData.component'
 import {ShowDataComponent} from './section/showData/showData.component'
 
+
+/* Definicion de Rutas */
+
+
+
+/* Definicion del componente root */
 @Component({
     selector: 'my-app',
     templateUrl: 'html/layout.template.html'
 })
 export class AppComponent {
 
-
 }
 
+
+/* Definicion del modulo principal */
 @NgModule({
-    imports: [BrowserModule],
+    imports: [BrowserModule, RouterModule.forRoot([
+        {path: '', redirectTo: '/add', pathMatch: 'full'},
+        {
+            path: 'add',
+            component: AddDataComponent
+        },
+        {
+            path: 'show',
+            component: ShowDataComponent
+        }
+    ])],
     declarations: [AppComponent, HeaderComponent, FooterComponent, AddDataComponent, ShowDataComponent],
     bootstrap: [AppComponent]
 })
